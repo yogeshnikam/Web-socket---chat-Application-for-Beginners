089183d7af2681cc3e683142c57622d719ba9768
Yogesh <yogeshnikam@users.noreply.github.com>
2017-09-25 10:22:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    })
+})
+
+http.listen(2222, function(){
+  console.log('listening on *:2222');
+});
+
+
+
+
+
